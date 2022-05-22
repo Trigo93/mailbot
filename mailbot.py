b7130d6c2a32b204e27b04efd93a84d5831ef0ae
Trigo93 <104326235+Trigo93@users.noreply.github.com>
2022-05-22 22:33:01 +0200

    Add modularity to mailbot

--- mailbot.py
+++ mailbot.py
@@ -8,17 +8,21 @@ EMAIL_SENDER = os.environ['EMAIL_SENDER']
 EMAIL_RECEIVER = os.environ['EMAIL_RECEIVER']
 EMAIL_PASSWORD = os.environ['EMAIL_SECRET']
 
-# Create an instance of the EmailMessage class
-msg = EmailMessage()# Define the 'Subject' of the email
-msg['Subject'] = 'Coucou Moumine'# Define 'From' (your email address)
-msg['From'] = EMAIL_SENDER # Define 'To' (to whom is it addressed)
-msg['To'] = EMAIL_RECEIVER # The email content (your message)
-msg.set_content('Test des boubins!')
+def process(attachment, subject, content):
+    # Create an instance of the EmailMessage class
+    msg = EmailMessage()# Define the 'Subject' of the email
+    msg['Subject'] = subject# Define 'From' (your email address)
+    msg['From'] = EMAIL_SENDER # Define 'To' (to whom is it addressed)
+    msg['To'] = EMAIL_RECEIVER # The email content (your message)
+    msg.set_content(content)
 
-with open('./photo.jpeg', 'rb') as attach:
-    msg.add_attachment(attach.read(), maintype='application', subtype='octet-stream', filename=attach.name)
+    with open(attachment, 'rb') as attach:
+        msg.add_attachment(attach.read(), maintype='application', subtype='octet-stream', filename=attach.name)
 
-# Establishing a secure connection (SSL), login to your email account and send the email
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(EMAIL_SENDER, EMAIL_PASSWORD) 
-    smtp.send_message(msg)
+    # Establishing a secure connection (SSL), login to your email account and send the email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_SENDER, EMAIL_PASSWORD) 
+        smtp.send_message(msg)
+
+if __name__ == "__main__":
+	process('/home/trigo/Documents/photo.jpeg', "Coucou", "Test")
