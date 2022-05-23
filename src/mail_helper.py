eb18a9ec9637f1485a766eb48ad4b1c0d785a3ac
Trigo93 <104326235+Trigo93@users.noreply.github.com>
2022-05-23 17:48:31 +0200

    clean up mail_helper.py

--- src/mail_helper.py
+++ src/mail_helper.py
@@ -1,19 +1,12 @@
-# Imports
-import os
 import smtplib
 from email.message import EmailMessage
 
-# Variables containing your email address and password
-EMAIL_SENDER = os.environ['EMAIL_SENDER']
-EMAIL_RECEIVER = os.environ['EMAIL_RECEIVER']
-EMAIL_PASSWORD = os.environ['EMAIL_SECRET']
-
-def process(attachment, subject, content):
+def process(sender, receiver, attachment, subject, content, password):
     # Create an instance of the EmailMessage class
     msg = EmailMessage()# Define the 'Subject' of the email
     msg['Subject'] = subject# Define 'From' (your email address)
-    msg['From'] = EMAIL_SENDER # Define 'To' (to whom is it addressed)
-    msg['To'] = EMAIL_RECEIVER # The email content (your message)
+    msg['From'] = sender # Define 'To' (to whom is it addressed)
+    msg['To'] = receiver # The email content (your message)
     msg.set_content(content)
 
     with open(attachment, 'rb') as attach:
@@ -21,8 +14,9 @@ def process(attachment, subject, content):
 
     # Establishing a secure connection (SSL), login to your email account and send the email
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(EMAIL_SENDER, EMAIL_PASSWORD) 
+        smtp.login(sender, password) 
         smtp.send_message(msg)
 
 if __name__ == "__main__":
-	process('photo.jpeg', "Coucou", "Test")
+	import os
+	process(os.environ['EMAIL_SENDER'], os.environ['EMAIL_RECEIVER'], 'photo.jpeg', "Coucou", "Test", os.environ['EMAIL_SECRET'])
