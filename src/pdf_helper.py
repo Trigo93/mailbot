c3ff05233acfe166874a83b3f2ea418de9fe296a
Trigo93 <104326235+Trigo93@users.noreply.github.com>
2022-06-14 23:06:58 +0200

    Factorize pdf_helper.py

--- src/pdf_helper.py
+++ src/pdf_helper.py
@@ -1,3 +1,4 @@
+import PyPDF2
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from PyPDF2.generic import DecodedStreamObject, EncodedStreamObject, NameObject
 
@@ -14,37 +15,28 @@ def replace_text(content, replacements = dict()):
 
     return result
 
+
 def process_data(object, replacements):
     data = object.getData()
+
     decoded_data = data.decode('iso-8859-1')
-
     replaced_data = replace_text(decoded_data, replacements)
-
     encoded_data = replaced_data.encode('iso-8859-1')
-    if object.decodedSelf is not None:
-        object.decodedSelf.setData(encoded_data)
-    else:
-        object.setData(encoded_data)
+
+    object.decodedSelf.setData(encoded_data)
 
 def process(file, replacements):
 
     pdf = PdfFileReader(file)
     writer = PdfFileWriter()
 
-    for page_number in range(pdf.getNumPages()):
+    page = pdf.getPage(0)
+    contents = page.getContents()
 
-        page = pdf.getPage(page_number)
-        contents = page.getContents()
+    process_data(contents, replacements)
 
-        if isinstance(contents, DecodedStreamObject) or isinstance(contents, EncodedStreamObject):
-            process_data(contents, replacements)
-        elif len(contents) > 0:
-            for obj in contents:
-                if isinstance(obj, DecodedStreamObject) or isinstance(obj, EncodedStreamObject):
-                    streamObj = obj.getObject()
-                    process_data(streamObj, replacements)
-
-        writer.addPage(page)
+    page[PyPDF2.pdf.NameObject("/Contents")] = contents.decodedSelf
+    writer.addPage(page)
 
     return writer
 
