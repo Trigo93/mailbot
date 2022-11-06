1b5f31a4e15d43f7a0764e337c7a2eed165bce92
Trigo93 <104326235+Trigo93@users.noreply.github.com>
2022-11-06 16:26:56 +0100

    add create api to pdf helper

--- src/pdf_helper.py
+++ src/pdf_helper.py
@@ -1,6 +1,7 @@
 import PyPDF2
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from PyPDF2.generic import DecodedStreamObject, EncodedStreamObject, NameObject
+from fpdf import FPDF
 
 def replace_text(content, replacements = dict()):
     lines = content.splitlines()
@@ -39,6 +40,76 @@ def process(file, replacements):
     writer.addPage(page)
 
     return writer
+
+def create(fname, data):
+    p = FPDF()
+    p.add_page()
+    p.set_font('Arial', 'B', 14)
+
+    p.cell(0, 5, 'LES BAILLEURS :', ln=1)
+    p.set_font('Arial', '', 12)
+    p.cell(0, 5, 'M. MALLET Tristan', ln=1)
+    p.cell(0, 5, 'Mme HOUDEBINE Lauriane', ln=1)
+    p.cell(0, 5, '95 avenue de Choisy', ln=1)
+    p.cell(0, 5, '75013, Paris', ln=1)
+    p.cell(0, 5, '06 62 79 84 81', ln=1)
+    p.cell(0, 5, '06 72 70 36 38', ln=1)
+
+
+    p.set_font('Arial', 'B', 14)
+    p.cell(0, 5, 'LES LOCATAIRES :', align="R", ln=1)
+
+    p.set_font('Arial', '', 12)
+    for name in data["names"]:
+        p.cell(0, 5, name, align="R", ln=1)
+    p.cell(0, 5, '16ter avenue Henri Barbusse', align="R", ln=1)
+    p.cell(0, 5, '94400, Vitry-Sur-Seine', align="R", ln=1)
+
+    p.ln(10)
+    p.set_font('Arial', 'B', 14)
+    p.cell(190, 20, 'QUITTANCE DE LOYER', border=True, align="C", ln=1)
+
+    p.ln(3)
+
+    p.set_font('Arial', '', 12)
+    p.cell(0, 5, 'Loyer du ' + data["dates"][0] + ' au ' + data["dates"][1], align="C", ln=1)
+
+    p.ln(3)
+
+    p.set_font('Arial', '', 11)
+    p.cell(0, 5, 'Reçu la somme de 1350 euros', ln=1)
+    p.cell(0, 5, 'Le ' + data["dates"][0], ln=1)
+    p.cell(0, 5, 'Pour loyer et accessoires des locaux sis :', ln=1)
+    p.cell(0, 5, '16ter avenue Henri Barbusse à Vitry-sur-Seine (94400) avec emplacement de parking libre, gardien et cave.', ln=5)
+
+    p.ln(10)
+
+    p.cell(0, 10, 'Détail de la quittance de loyer', border="T", ln=1)
+    p.cell(0, 5, '- Loyer hors charges : 1151 euros', ln=1)
+    p.cell(0, 5, '- Charges : 199 euros', ln=1)
+
+    p.ln(3)
+
+    p.set_font('Arial', 'B', 12)
+    p.cell(0, 10, 'TOTAL : 1350 euros', border="B", ln=1)
+
+    p.ln(15)
+
+    p.set_font('Arial', '', 11)
+    p.cell(0, 5, 'Fait à Paris le ' + data["dates"][0], ln=1)
+    p.cell(0, 5, 'Signatures :', ln=1)
+
+    for i, file in enumerate(data["signatures"]):
+        # TODO: Parametrize signature location according to jpeg size
+        p.image('signature_' + str(i+1) + ".jpg", x = 20 + 30*i, y = 220 - 20*i, w = 30, h = 0)
+
+    # Go to 4 cm from bottom
+    p.set_y(-40)
+    p.set_font('Arial', 'I', 8)
+    p.cell(0, 3, "Le paiement de la présente n'emporte pas présomption de paiement des termes antérieurs.", ln=1, align="C")
+    p.cell(0, 3, 'Cette quittance ou ce reçu annule tous les reçus qui auraient pu être donnés pour acompte versé sur le présent terme.', ln=1, align="C")
+    p.cell(0, 3, "En cas de congé précédemment donné, cette quittance ou ce reçu représenterait l'indemnité d'occupation", ln=1, align="C")
+    p.cell(0, 3, "et ne saurait être considéré comme un titre d'occupation. Sous réserve d'encaissement.", ln=1, align="C")
 
 
 if __name__ == "__main__":
